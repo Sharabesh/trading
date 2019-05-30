66a7d5fbf02a24f98bad424e6634f85465bcece8
Sharabesh Ramesh <sharabesh@berkeley.edu>
2019-05-29 20:41:34 -0700

    some intro code

--- src/auth.py
+++ src/auth.py
@@ -4,3 +4,14 @@ Initializes control logic Alpaca,
 
 
 """
+import alpaca_trade_api as tradeapi
+import os
+
+# Initialize keys from the OS environment
+try:
+    KEY_ID = os.environ['ALPACA_KEY_ID']
+    ALPACA_SECRET_KEY = os.environ['ALPACA_SECRET_KEY']
+except:
+    print("Keys not initialized in the environment please set the \"ALPACA_KEY_ID\" and \"ALPACA_SECRET_KEY\" environment variables")
+    exit(-1)
+
